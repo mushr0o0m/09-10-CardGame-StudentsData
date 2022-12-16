5d0c8057021574fe97a80bfa8987fa5a600cab0f
mushr0o0m <105949115+mushr0o0m@users.noreply.github.com>
2022-12-16 06:28:39 +0500

    Add some fix

--- 10/students-data.js
+++ 10/students-data.js
@@ -11,36 +11,101 @@
 
             fullName.textContent = [student.name, student.surname, student.middleName].join(" ");
             department.textContent = student.department;
-            birthDate.textContent = getCorrectBirthDayInfo(student.birthDate);
-            console.log(new Date().getFullYear() - student.studyStart)
-            studyStart.textContent = `${student.studyStart} (${(new Date().getFullYear() - student.studyStart) + (new Date().getMonth() > 8 ? 1 : 0)} курс)`;
+            birthDate.textContent = getCorrectBirthDateInfo(student.birthDate);
+            studyStart.textContent = getCorrectStudyStartDateInfo(student.studyStart);
 
             row.append(fullName, department, birthDate, studyStart);
-            console.log(row);
             table.append(row);
         }
 
     }
 
-    function getCorrectBirthDayInfo(birthDate) {
+    function getCorrectBirthDateInfo(birthDate) {
         const yearsNum = new Date().getFullYear() - birthDate.getFullYear();
         const correctMonth = (birthDate.getMonth() + 1 < 10 ? "0" : "") + (birthDate.getMonth() + 1);
         const correctBirthDate = `${birthDate.getDate()}.${correctMonth}.${birthDate.getFullYear()}`;
         return `${correctBirthDate} (${yearsNum} лет)`
     }
 
+    function getCorrectStudyStartDateInfo(studyStart) {
+        const courseNum = (new Date().getFullYear() - studyStart)
+            + (new Date().getMonth() > 8 ? 1 : 0);
+        return `${studyStart}-${studyStart + 4} (${courseNum} курс)`;
+    }
+
     function createStudentsData() {
         let students = []
         const addStudentBtn = document.getElementById("addStudent");
         addStudentBtn.addEventListener("click", () => {
-            students.push(addStudent())
-            generateTable(students);
+            const form = document.forms.student;
+            if (validateForm(form)) {
+                students.push(addStudent(form));
+                generateTable(students);
+            }
         });
     }
 
-    function addStudent() {
-        const form = document.forms.student;
+    function validateBirthDate(birthDateStr) {
+        const birthDate = new Date(birthDateStr);
+        const dateNow = new Date();
+        return (dateNow.getFullYear() - birthDate.getFullYear() > 0)
+            || (dateNow.getFullYear() - birthDate.getFullYear() === 0
+                && dateNow.getMonth() - birthDate.getMonth() > 0)
+            || (dateNow.getFullYear() - birthDate.getFullYear() === 0
+                && dateNow.getMonth() - birthDate.getMonth() === 0
+                && dateNow.getDay() - birthDate.getDay() >= 0);
+    }
 
+    function validateStudyStart(studyStart) {
+        const dateNow = new Date();
+        return !isNaN(studyStart) && studyStart.length === 4
+            && studyStart - 2000 > 0 && dateNow.getFullYear() - studyStart >= 0;
+    }
+
+    function validateForm(form) {
+        const btn = form.querySelector("button");
+        const errorMessage = document.createElement("p");
+        const birthDate = form.elements.birthDate.value;
+        const studyStart = form.elements.studyStart.value;
+        let errorMessageText = "";
+        errorMessage.classList.add("text-danger", "mb-3", "text-left")
+        let isCorrect = true;
+        for (let element of form.querySelectorAll("div > input")) {
+            if (element.value.trim() === "") {
+                errorMessageText += "Все поля должны быть заполнены! ";
+                isCorrect = false;
+                break;
+            }
+        }
+
+        if (!validateBirthDate(birthDate)) {
+            isCorrect = false;
+            errorMessageText += (errorMessageText === "" ? "" : "<br>")
+                + "Введите коректную дату рождения! ";
+        }
+
+        if (!validateStudyStart(studyStart)) {
+            isCorrect = false;
+            errorMessageText += (errorMessageText === "" ? "" : "<br>")
+                + "Введите корректный год начала обучения! ";
+        }
+
+        if (isCorrect && form.querySelector("p") != null)
+            form.querySelector("p").remove();
+        else if (!isCorrect) {
+            if (form.querySelector("p") === null) {
+                errorMessage.innerHTML = errorMessageText;
+                form.insertBefore(errorMessage, btn);
+            }
+            else
+                form.querySelector("p").innerHTML = errorMessageText;
+
+        }
+
+        return isCorrect;
+    }
+
+    function addStudent(form) {
         const name = form.elements.name;
         const surname = form.elements.surname;
         const middleName = form.elements.middleName;
